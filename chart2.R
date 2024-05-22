4b13edfc022f709d48640f75c02ee558e822f259
bkebede1 <bkebede1@uw.edu>
2024-05-22 04:43:21 -0700

    Fully Completed

--- chart2.R
+++ chart2.R
@@ -7,15 +7,12 @@ stats <- read.csv("stats_dataset.csv")
 stats <- stats %>% replace(is.na(.), 0)
 main_stats <- stats %>% 
   select(26, 21, 22, 23, 24, 20)
+
 averages <- colMeans(main_stats)
 averages <- round(averages, digits = 2)
 averages <- as.data.frame(averages)
 names <- row.names(averages)
 avg <- cbind(as.data.frame(averages), as.data.frame(names))
 
-#FG <- list(fg$Pos)
-#y <- aggregate(PTS~TRB, data = main_stats, FUN=mean)
-#barplot(mean, ylab = "Average", main = "Average Stats in the NBA & WNBA")
-#ggplot(avg, aes(x=avg,fill=avg)) + labs(y="Average") + ggtitle("Average Stats in the NBA & WNBA")
 ggplot(avg, aes(x=names, y=averages,fill=names)) + geom_bar(stat="identity") + labs( y="Average") + 
   ggtitle("Average Stats in the NBA & WNBA") + geom_text(aes(label=averages),position="stack",vjust=1)
---
+++ summary.R
@@ -0,0 +1,16 @@
+library("plotly")
+library("readr")
+setwd("C:\\Users\\Binyam\\Downloads")
+stats <- read.csv("stats_dataset.csv")
+stats <- stats %>% replace(is.na(.), 0)
+
+stats_summary <- stats %>%
+  group_by(Pos) %>%
+  summarize(
+    mean_ppg = mean(PTS),
+    mean_ast = mean(AST),
+    mean_stl = mean(STL),
+    mean_trb = mean(TRB),
+    mean_tov = mean(TOV)
+  )
+
